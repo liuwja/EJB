bc0409365ddcf83d7ace578d51c5cecb7870cb1e
liuwjg <liuwja@outlook.com>
2020-08-02 00:24:31 +0800

    持久化bean第一次提交

---
+++ entity-bean/src/com/liuwjg/bean/Person.java
@@ -0,0 +1,30 @@
+package com.liuwjg.bean;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Person {
+    private Integer id;
+    private String name;
+
+    @Column(name = "id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
